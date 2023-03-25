30ceac5da79dae1216843b3c1884ea1be9466bea
svkolesnikov <svkolesnikov@scid.ru>
2023-03-25 16:03:33 +0300

    svyat: модуль 4 Во второй задаче добавил функции параметр, можно сортировать  любые переданные списки

--- module_4/m42.py
+++ module_4/m42.py
@@ -1,15 +1,14 @@
 # Сортировка методом вставки
 def sortInsert(ar):
-
+    ar = ar.copy()
     for i in range(1, len(ar)):
         j = i
         while (j > 0) & (ar[j] < ar[j-1]):
             ar[j-1], ar[j] = ar[j], ar[j-1]
             j -= 1
-    pass
+    return ar
 
 
 l = [8, 1, 5, 2, 6, 9, 3]
 print(l)
-sortInsert(l)
-print(l)
+print(sortInsert(l))
