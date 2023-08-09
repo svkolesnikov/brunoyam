6beb9b3a15a830f7e2f44c5fef3fac77702f3755
svkolesnikov <svkolesnikov@scid.ru>
2023-08-09 07:43:55 +0300

    Переименован проект

--- pythonProject/shop/main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import BdUser, BdHome, BdCompany
-
-admin.site.register(BdUser)
-admin.site.register(BdHome)
-admin.site.register(BdCompany)
--- pythonProject/shop/main/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('index', views.index, name="Index"),
-    path('info', views.info, name="Info"),
-]
